309a714ad035734de689a81212bbf20fde44f4d9
KudwanMayank <m.kudwan@iitg.ac.in>
2024-06-28 03:55:29 +0530

    Updated Frontend

--- connect4.js
+++ connect4.js
@@ -1,5 +1,134 @@
 
 
+// var playerRed = "R";
+// var playerYellow = "Y";
+// var currPlayer = playerRed;
+
+// var gameOver = false;
+// var board;
+
+// var rows = 6;
+// var columns = 7;
+// var currColumns = []; //keeps track of which row each column is at.
+
+// window.onload = function() {
+//     setGame();
+// }
+
+// function setGame() {
+//     board = [];
+//     currColumns = [5, 5, 5, 5, 5, 5, 5];
+
+//     for (let r = 0; r < rows; r++) {
+//         let row = [];
+//         for (let c = 0; c < columns; c++) {
+//             // JS
+//             row.push(' ');
+//             // HTML
+//             let tile = document.createElement("div");
+//             tile.id = r.toString() + "-" + c.toString();
+//             tile.classList.add("tile");
+//             tile.addEventListener("click", setPiece);
+//             document.getElementById("board").append(tile);
+//         }
+//         board.push(row);
+//     }
+// }
+
+// function setPiece() {
+//     if (gameOver) {
+//         return;
+//     }
+
+//     //get coords of that tile clicked
+//     let coords = this.id.split("-");
+//     let r = parseInt(coords[0]);
+//     let c = parseInt(coords[1]);
+
+//     // figure out which row the current column should be on
+//     r = currColumns[c]; 
+
+//     if (r < 0) { // board[r][c] != ' '
+//         return;
+//     }
+
+//     board[r][c] = currPlayer; //update JS board
+//     let tile = document.getElementById(r.toString() + "-" + c.toString());
+//     if (currPlayer == playerRed) {
+//         tile.classList.add("red-piece");
+//         currPlayer = playerYellow;
+//     }
+//     else {
+//         tile.classList.add("yellow-piece");
+//         currPlayer = playerRed;
+//     }
+
+//     r -= 1; //update the row height for that column
+//     currColumns[c] = r; //update the array
+
+//     checkWinner();
+// }
+
+// function checkWinner() {
+//      // horizontal
+//      for (let r = 0; r < rows; r++) {
+//          for (let c = 0; c < columns - 3; c++){
+//             if (board[r][c] != ' ') {
+//                 if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+//                     setWinner(r, c);
+//                     return;
+//                 }
+//             }
+//          }
+//     }
+
+//     // vertical
+//     for (let c = 0; c < columns; c++) {
+//         for (let r = 0; r < rows - 3; r++) {
+//             if (board[r][c] != ' ') {
+//                 if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]) {
+//                     setWinner(r, c);
+//                     return;
+//                 }
+//             }
+//         }
+//     }
+
+//     // anti diagonal
+//     for (let r = 0; r < rows - 3; r++) {
+//         for (let c = 0; c < columns - 3; c++) {
+//             if (board[r][c] != ' ') {
+//                 if (board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]) {
+//                     setWinner(r, c);
+//                     return;
+//                 }
+//             }
+//         }
+//     }
+
+//     // diagonal
+//     for (let r = 3; r < rows; r++) {
+//         for (let c = 0; c < columns - 3; c++) {
+//             if (board[r][c] != ' ') {
+//                 if (board[r][c] == board[r-1][c+1] && board[r-1][c+1] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3]) {
+//                     setWinner(r, c);
+//                     return;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// function setWinner(r, c) {
+//     let winner = document.getElementById("winner");
+//     if (board[r][c] == playerRed) {
+//         winner.innerText = "Red Wins";             
+//     } else {
+//         winner.innerText = "Yellow Wins";
+//     }
+//     gameOver = true;
+// }
+
 var playerRed = "R";
 var playerYellow = "Y";
 var currPlayer = playerRed;
@@ -13,6 +142,7 @@ var currColumns = []; //keeps track of which row each column is at.
 
 window.onload = function() {
     setGame();
+    updateTurnDisplay();
 }
 
 function setGame() {
@@ -57,8 +187,7 @@ function setPiece() {
     if (currPlayer == playerRed) {
         tile.classList.add("red-piece");
         currPlayer = playerYellow;
-    }
-    else {
+    } else {
         tile.classList.add("yellow-piece");
         currPlayer = playerRed;
     }
@@ -66,20 +195,21 @@ function setPiece() {
     r -= 1; //update the row height for that column
     currColumns[c] = r; //update the array
 
+    updateTurnDisplay();
     checkWinner();
 }
 
 function checkWinner() {
-     // horizontal
-     for (let r = 0; r < rows; r++) {
-         for (let c = 0; c < columns - 3; c++){
+    // horizontal
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++){
             if (board[r][c] != ' ') {
                 if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
                     setWinner(r, c);
                     return;
                 }
             }
-         }
+        }
     }
 
     // vertical
@@ -127,4 +257,13 @@ function setWinner(r, c) {
         winner.innerText = "Yellow Wins";
     }
     gameOver = true;
+}
+
+function updateTurnDisplay() {
+    let turnDisplay = document.getElementById("currentTurn");
+    if (currPlayer == playerRed) {
+        turnDisplay.innerText = "Red's Turn";
+    } else {
+        turnDisplay.innerText = "Yellow's Turn";
+    }
 }
